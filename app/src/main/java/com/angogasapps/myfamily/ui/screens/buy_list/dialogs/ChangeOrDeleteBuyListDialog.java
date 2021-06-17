0b42be49fb31dc2e168351fde82a3691ffeabb2d
AnGoga <Anogeorgy05@gmail.com>
2021-06-17 17:05:18 +0300

    Список покупок -> диалоги

--- app/src/main/java/com/angogasapps/myfamily/ui/screens/buy_list/dialogs/ChangeOrDeleteBuyListDialog.java
+++ app/src/main/java/com/angogasapps/myfamily/ui/screens/buy_list/dialogs/ChangeOrDeleteBuyListDialog.java
@@ -15,53 +15,59 @@ import com.angogasapps.myfamily.models.buy_list.BuyList;
 
 import es.dmoral.toasty.Toasty;
 
-public class ChangeOrDeleteBuyListDialog extends AlertDialog {
-    private ChangeOrDeleteDialogBinding binding;
+public class ChangeOrDeleteBuyListDialog {
     private BuyList buyList;
+    private Context context;
+    private AlertDialog dialog;
 
     public ChangeOrDeleteBuyListDialog(@NonNull Context context, BuyList buyList) {
-        super(context);
+        this.context = context;
         this.buyList = buyList;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ChangeOrDeleteDialogBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        binding.editBtn.setOnClickListener(this::onClickEditButton);
-        binding.removeBtn.setOnClickListener(this::onClickRemoveButton);
+    public void show() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        String[] list = {context.getString(R.string.rename), context.getString(R.string.remove)};
+        builder.setItems(list, (dialog, which) -> {
+            if (which == 0){
+                onClickEditButton(null);
+            } else if(which == 1){
+                onClickRemoveButton(null);
+            }
+        });
+        this.dialog = builder.create();
+        dialog.show();
     }
 
     public void onClickEditButton(View view){
-        (new AddBuyListDialog(getContext(), buyList)).show();
-        this.dismiss();
+        (new AddBuyListDialog(context, buyList)).show();
+        this.dialog.dismiss();
     }
 
     public void onClickRemoveButton(View view){
-        AlertDialog dialog = new Builder(getContext())
+        AlertDialog dialog = new AlertDialog.Builder(context)
                 .setTitle(R.string.remove_buy_list)
-                .setMessage(getContext().getString(R.string.change_or_delet_buy_list_dialog_text1)+ "\"" + buyList.getName() + "\" ?"
-                    + "\n" + getContext().getString(R.string.change_or_delete_product_dialog_text2))
+                .setMessage(context.getString(R.string.change_or_delet_buy_list_dialog_text1)+ "\"" + buyList.getName() + "\" ?"
+                    + "\n" + context.getString(R.string.change_or_delete_product_dialog_text2))
                 .setPositiveButton(R.string.remove, (dialog1, which) -> {
-                    if (which != BUTTON_POSITIVE) return;
+                    if (which != AlertDialog.BUTTON_POSITIVE) return;
                     BuyListFunks.deleteBuyList(buyList, new IOnEndCommunicationWithFirebase() {
                         @Override
                         public void onSuccess() { /*TODO: . . .*/}
 
                         @Override
                         public void onFailure() {
-                            Toasty.error(getContext(), R.string.something_went_wrong).show();
+                            Toasty.error(context, R.string.something_went_wrong).show();
                         }
                     });
                 })
                 .setNegativeButton(R.string.cancel, (dialog1, which) -> {
-                    if (which != BUTTON_NEGATIVE) return;
+                    if (which != AlertDialog.BUTTON_NEGATIVE) return;
                     dialog1.dismiss();
                 })
                 .create();
         dialog.show();
-        this.dismiss();
+        this.dialog.dismiss();
     }
 }
--- app/src/main/java/com/angogasapps/myfamily/ui/screens/buy_list/dialogs/ChangeOrDeleteProductDialog.java
+++ app/src/main/java/com/angogasapps/myfamily/ui/screens/buy_list/dialogs/ChangeOrDeleteProductDialog.java
@@ -1,73 +1,70 @@
 package com.angogasapps.myfamily.ui.screens.buy_list.dialogs;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 
 import com.angogasapps.myfamily.R;
-import com.angogasapps.myfamily.databinding.ChangeOrDeleteDialogBinding;
 import com.angogasapps.myfamily.firebase.BuyListFunks;
 import com.angogasapps.myfamily.firebase.interfaces.IOnEndCommunicationWithFirebase;
 import com.angogasapps.myfamily.models.buy_list.BuyList;
 
-public class ChangeOrDeleteProductDialog extends AlertDialog {
-    private ChangeOrDeleteDialogBinding binding;
+public class ChangeOrDeleteProductDialog {
     private BuyList.Product product;
     private String buyListId;
+    private Context context;
+    private AlertDialog dialog;
 
     public ChangeOrDeleteProductDialog(@NonNull Context context, String buyListId, BuyList.Product product) {
-        super(context);
+        this.context = context;
         this.buyListId = buyListId;
         this.product = product;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ChangeOrDeleteDialogBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        binding.editBtn.setOnClickListener(this::onClickEditBtn);
-        binding.removeBtn.setOnClickListener(this::onClickRemoveBtn);
+    public void show() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        String[] list = {context.getString(R.string.rename), context.getString(R.string.remove)};
+        builder.setItems(list, (dialog, which) -> {
+            if (which == 0){
+                onClickEditButton(null);
+            } else if(which == 1){
+                onClickRemoveButton(null);
+            }
+        });
+        this.dialog = builder.create();
+        dialog.show();
+
     }
 
-    public void onClickRemoveBtn(View view){
-        AlertDialog dialog = new Builder(getContext())
+    public void onClickRemoveButton(View view){
+        AlertDialog dialog = new AlertDialog.Builder(context)
                 .setTitle(R.string.remove_product)
-                .setMessage(getContext().getString(R.string.change_or_delete_product_dialog_text1) + "\"" + product.getName() + "\"" + "?" +
-                        getContext().getString(R.string.change_or_delete_product_dialog_text2))
+                .setMessage(context.getString(R.string.change_or_delete_product_dialog_text1) + "\"" + product.getName() + "\"" + "?" +
+                        context.getString(R.string.change_or_delete_product_dialog_text2))
                 .setPositiveButton(R.string.remove, (dialog1, which) -> {
-                    if (which != BUTTON_POSITIVE) return;
+                    if (which != AlertDialog.BUTTON_POSITIVE) return;
 
-                    getButton(BUTTON_POSITIVE).setClickable(false);
                     BuyListFunks.deleteProduct(buyListId, product, new IOnEndCommunicationWithFirebase() {
-                        @Override
-                        public void onSuccess() {
-                            getButton(BUTTON_POSITIVE).setClickable(true);
-                        }
-
-                        @Override
-                        public void onFailure() {
-                            getButton(BUTTON_POSITIVE).setClickable(true);
-                        }
+                        @Override public void onSuccess() {}
+                        @Override public void onFailure() {}
                     });
 
                 })
                 .setNegativeButton(R.string.cancel, (dialog1, which) -> {
-                    if (which != BUTTON_NEGATIVE) return;
+                    if (which != AlertDialog.BUTTON_NEGATIVE) return;
                     dialog1.dismiss();
                 })
                 .create();
 
         dialog.show();
-        this.dismiss();
+        this.dialog.dismiss();
     }
 
-    public void onClickEditBtn(View view){
-        (new BuyListProductCreatorDialog(getContext(), buyListId, product)).show();
-        this.dismiss();
+    public void onClickEditButton(View view){
+        (new BuyListProductCreatorDialog(context, buyListId, product)).show();
+        this.dialog.dismiss();
     }
 }
