33ebc866d2cdb3252b3cb5cfea1f2dbf8c812666
AnGoga <Anogeorgy05@gmail.com>
2021-07-08 00:54:25 +0300

    FamilyClock -> 7

--- app/src/main/java/com/angogasapps/myfamily/ui/screens/family_clock/ClockWakeUpperManager.kt
+++ app/src/main/java/com/angogasapps/myfamily/ui/screens/family_clock/ClockWakeUpperManager.kt
@@ -4,9 +4,11 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.provider.AlarmClock
 import androidx.core.content.ContextCompat.getSystemService
 import com.angogasapps.myfamily.app.AppApplication
 import com.angogasapps.myfamily.models.family_clock.ClockObject
+import java.util.*
 
 
 class ClockWakeUpperManager private constructor(){
@@ -31,4 +33,24 @@ class ClockWakeUpperManager private constructor(){
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager[AlarmManager.RTC_WAKEUP, obj.time] = pendingIntent
     }
+
+//    @Synchronized
+//    fun wakeUpClock(obj: ClockObject, context: Context) {
+//        var delta = (obj.time - System.currentTimeMillis()) / 1000
+//        if (delta <= 0) return
+//
+//        val days = (delta - (delta % 86_400)) / 86_400
+//        delta -= days * 86_400
+//        val hours = (delta - (delta % 3600)) / 3600
+//        delta -= hours * 3600
+//        val minutes = (delta - (delta % 60)) / 60
+//
+//        val intent = Intent(AlarmClock.ACTION_SET_ALARM)
+//        intent.putExtra(AlarmClock.EXTRA_DAYS, days)
+//        intent.putExtra(AlarmClock.EXTRA_HOUR, hours)
+//        intent.putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+//        intent.putExtra(AlarmClock.EXTRA_SKIP_UI, false)
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//        context.startActivity(intent)
+//    }
 }
--- app/src/main/java/com/angogasapps/myfamily/ui/screens/family_clock/FamilyClockReceiver.kt
+++ app/src/main/java/com/angogasapps/myfamily/ui/screens/family_clock/FamilyClockReceiver.kt
@@ -1,19 +1,62 @@
 package com.angogasapps.myfamily.ui.screens.family_clock
 
+import android.R
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.angogasapps.myfamily.ui.screens.splash.SplashActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.angogasapps.myfamily.app.AppNotificationManager.CHANNEL_ID
+
 
 class FamilyClockReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        println(intent.toString() + Thread.currentThread())
 
-        val intent = Intent(context, AlarmClockActivity::class.java)
-                .also { it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); }
+//        val intent = Intent(context, AlarmClockActivity::class.java)
+//                .also { it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); }
+//
+//        context.startActivity(intent)
 
-        context.startActivity(intent)
+        val fullScreenIntent = Intent(context, AlarmClockActivity::class.java)
+        val fullScreenPendingIntent = PendingIntent.getActivity(context, 0,
+                fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
+        val builder =
+                NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.sym_def_app_icon)
+                        .setContentTitle("Incoming call")
+                        .setContentText("(919) 555-1234")
+                        .setPriority(NotificationCompat.PRIORITY_MAX)
+                        .setCategory(NotificationCompat.CATEGORY_CALL)
+                        .setFullScreenIntent(fullScreenPendingIntent, true)
+
+
+        NotificationManagerCompat.from(context).notify(115, builder.build())
     }
 }
+
+/*
+        AppNotificationManager.createNotificationChanel(context)
+
+        val builder: NotificationCompat.Builder = Builder(context)
+                .setSmallIcon(R.drawable.btn_star)
+                .setContentTitle("This is title of notification")
+                .setContentText("This is a notification Text")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_CALL)
+                .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.mipmap.sym_def_app_icon))
+//                .setAutoCancel(true)
+
+        val intent = Intent(context, AlarmClockActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 113, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        builder.setContentIntent(pendingIntent)
+
+        builder.setFullScreenIntent(pendingIntent, true)
+
+
+        val manager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(115, builder.build())
+*/
